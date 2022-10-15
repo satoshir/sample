119b75722080a9067ca567b13eb1438687c8c1d2
satoshi@com <ikezawa@com>
2022-10-15 22:55:04 +0900

    10/15

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,21 @@
+class ApplicationController < ActionController::Base
+
+    # deviseコントローラーにストロングパラメータを追加する          
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    def after_sign_in_path_for(resource)
+    #   home_index_path
+    end
+  
+    def after_sign_out_path_for(resource)
+      user_session_path
+    end
+  
+    protected
+  
+    def configure_permitted_parameters
+      # サインアップ時にnameとphone_numberのストロングパラメータを追加
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+  
+  end
