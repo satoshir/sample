711a2d11f815880b86aadc0a944e8c7977da3c96
satoshi@com <ikezawa@com>
2022-10-17 23:42:12 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,8 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       # サインアップ時にnameとphone_numberのストロングパラメータを追加
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      # 情報更新時にnicknameの取得を許可
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
   
   end
